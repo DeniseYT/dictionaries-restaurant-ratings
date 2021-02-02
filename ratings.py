391963b992331863100cd83ffb869b2e9af99cad
Denise <denise.ytlin@gmail.com>
2021-02-02 22:14:31 +0000

    get_rating

--- ratings.py
+++ ratings.py
@@ -1,3 +1,22 @@
-
+def get_rating(file):
     """Restaurant rating lister."""
+
+    ratings = open(file)
+    restaurant_rating = {}
+
+    for line in ratings:
+        line = line.rstrip()
+        items = line.split(" ")
+
+        for item in items:
+            restaurant_rating[item] += 1
+    
+    for key, value in restaurant_rating.items():
+        print(f"{key} is rated at {value}")
+        print(key, value)
+    
+    return restaurant_rating
+    
+print(get_rating("scores.txt"))
+
 
