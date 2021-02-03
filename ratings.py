a924da5aa77eea866eba1417baf2cf80eaa62f47
Denise <denise.ytlin@gmail.com>
2021-02-03 20:19:12 +0000

    add_rating

--- ratings.py
+++ ratings.py
@@ -1,3 +1,4 @@
+
 def get_rating(file):
     """Restaurant rating lister."""
 
@@ -8,13 +9,30 @@ def get_rating(file):
         line = line.rstrip()
         restaurant, score = line.split(":")
         restaurant_rating[restaurant] = int(score)
-    print(restaurant_rating)
     
-    for key, value in restaurant_rating.items():
+    for key, value in sorted(restaurant_rating.items()):
         print(f"{key} is rated at {value}")
         
     return restaurant_rating
-    
+  
 get_rating("scores.txt")
+
+
+def add_rating(restaurant_rating):
+
+    new_restaurant = input("Which restaurant you'd like to rate? > ")
+    new_restaurant = new_restaurant.title()
+    new_score = input("What's the rating? > ")
+
+    restaurant_rating[new_restaurant] = int(new_score)
+
+    for key, value in sorted(restaurant_rating.items()):
+        print(f"{key} is rated at {value}")
+
+    return restaurant_rating
+
+add_rating(get_rating("scores.txt"))
+
+
 
 
